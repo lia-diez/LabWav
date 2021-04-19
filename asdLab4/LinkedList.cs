5fe7b4034952b890592f20d1dc6df425f53f00e2
ASharp <a.sharp.fus@gmail.com>
2021-04-19 20:57:18 +0300

    zria ti xml ne zacomitil....

---
+++ asdLab4/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace asdLab4
+{
+    public class Element
+    {
+        public int Data;
+        public readonly Element Previous;
+
+        public Element(int data, Element previous)
+        {
+            Data = data;
+            Previous = previous;
+        }
+    }
+
+    public struct LinkedList
+    {
+        public Element Tail;
+
+        public void Add(int data)
+        {
+            if (Tail == null)
+            {
+                Tail = new Element(data, null);
+            }
+            else
+            {
+                Element newElement = new Element(data, Tail);
+                Tail = newElement;
+            }
+        }
+
+        public void Print()
+        {
+            Element current = Tail;
+            string result = "";
+            do
+            {
+                string newstr = $"{current.Data}\t";
+                newstr += result;
+                result = newstr;
+                current = current.Previous;
+            } while (current != null);
+
+            Console.WriteLine(result);
+        }
+
+        public void Remove(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Tail = Tail.Previous;
+            }
+        }
+    }
+}
---
+++ asdLab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace asdLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter few integer numbers. When all numbers are entered, type 'end'");
+            LinkedList linkedList = new LinkedList();
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                if (int.TryParse(input, out int number)) linkedList.Add(number);
+                else Console.WriteLine("Wrong input. Please, enter integer numbers");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Input list is:");
+            linkedList.Print();
+            int sum = linkedList.Tail.Data + linkedList.Tail.Previous.Data;
+            
+            // listInt.Remove(2); 
+            // listInt.Add(sum);
+            // listInt.Add(sum);
+            
+            if (linkedList.Tail.Previous != null)
+            {
+                linkedList.Tail.Data = sum;
+                linkedList.Tail.Previous.Data = sum;
+                Console.WriteLine("Changed list is:");
+                linkedList.Print();
+            }
+        }
+    }
+}
